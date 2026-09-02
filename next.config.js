26cef4af8f55d032990b2af8021c0d0d7f78eb80
Ahana <mysto9245@gmail.com>
2026-09-02 03:24:37 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -9,9 +9,10 @@ const nextConfig = {
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
   },
-  experimental: {
-    optimizeCss: true,
-  },
+  // Remove optimizeCss if causing issues
+  // experimental: {
+  //   optimizeCss: true,
+  // },
   async headers() {
     return [
       {
