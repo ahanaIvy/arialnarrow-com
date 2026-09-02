05bc106436ad2a0ad49ad56d64cde08780f58498
Ahana <mysto9245@gmail.com>
2026-09-02 03:37:12 -0400

    Update Container.tsx

--- app/components/ui/Container.tsx
+++ app/components/ui/Container.tsx
@@ -6,9 +6,5 @@ interface ContainerProps {
 }
 
 export function Container({ children, className = '' }: ContainerProps) {
-  return (
-    <div className={`container mx-auto px-4 sm:px-6 lg:px-8 ${className}`}>
-      {children}
-    </div>
-  );
+  return <div className={`container mx-auto px-4 sm:px-6 lg:px-8 ${className}`}>{children}</div>;
 }
